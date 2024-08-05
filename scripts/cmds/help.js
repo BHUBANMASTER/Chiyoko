e965ecb87946e3b8fb1fea6f1ab20fa33b404de2
BHUBANMASTER <hackesport8278@gmail.com>
2024-08-05 16:49:29 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 👽| 𝘼𝙣𝙘𝙝𝙚𝙨𝙩𝙤𝙧 𝘼𝙞 ]";
+const doNotDelete = "[ ~ 𝘾𝙃𝙄𝙔𝙊𝙆𝙊 ♡࿐ ]";
  
 module.exports = {
   config: {
@@ -34,7 +34,7 @@ module.exports = {
       const categories = {};
       let msg = "";
  
-      msg += `╔══════════════╗\n   ✿︎👽| 𝘼𝙣𝙘𝙝𝙚𝙨𝙩𝙤𝙧 𝘼𝙞 ✿︎\n╚══════════════╝`;
+      msg += `╔══════════════╗\n   ✿︎ ~ 𝘾𝙃𝙄𝙔𝙊𝙆𝙊 ♡࿐ ✿︎\n╚══════════════╝`;
  
       for (const [name, value] of commands) {
           if (value.config.role > 1 && role < value.config.role) continue;
